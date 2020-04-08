04c2ef7b37fb0fc3387e0a67381f68c8a4eec036
Soren-Frederiksen <62955602+Soren-Frederiksen@users.noreply.github.com>
2020-04-08 13:31:21 -0600

    You're doing great

--- hello.go
+++ hello.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("\nHello World\nThis is a test\n")
+	foo()
+	fmt.Println("Something else")
+	for i := 0; i<50; i++ {
+		if i %5 == 0{
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
 }
